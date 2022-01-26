f5a346966e2c95950709ee099d80ccbb0c0e7cc0
Sohini Makar <sohini.makar@gmail.com>
2022-01-26 14:22:49 -0800

    implement Pick<T, K>

---
+++ MyPick.ts
@@ -0,0 +1,19 @@
+/*
+implement Pick<T, K>
+
+Pick<T, K>, as the name implies, returns a new type by picking properties in K from T.
+
+Please implement MyPick<T, K> by yourself.
+
+type Foo = {
+  a: string
+  b: number
+  c: boolean
+}
+
+type A = MyPick<Foo, 'a' | 'b'> // {a: string, b: number}
+type B = MyPick<Foo, 'c'> // {c: boolean}
+type C = MyPick<Foo, 'd'> // Error
+*/
+
+type MyPick<T, K extends keyof T> = {[k in K]: T[k]};
