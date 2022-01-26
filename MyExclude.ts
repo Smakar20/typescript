8fadec2933d6bef28e16f01ec990ef8bf9237ec3
Sohini Makar <sohini.makar@gmail.com>
2022-01-26 14:33:27 -0800

    implement Exclude<T, E>

---
+++ MyExclude.ts
@@ -0,0 +1,16 @@
+/*
+ implement Exclude<T, E>
+ 
+ Exclude<T, K> returns a type by removing from T the union members that are assignable to K.
+
+Please implement MyExclude<T, K> by yourself.
+
+type Foo = 'a' | 'b' | 'c'
+
+type A = MyExclude<Foo, 'a'> // 'b' | 'c'
+type B = MyExclude<Foo, 'c'> // 'a' | 'b
+type C = MyExclude<Foo, 'c' | 'd'>  // 'a' | 'b'
+type D = MyExclude<Foo, 'a' | 'b' | 'c'>  // never
+*/
+
+type MyExclude<T, E> = T extends E ? never : T;
